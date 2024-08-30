d1a0c8c51d800c15b241e40a0079891b64b14f71
dbotti <davide.botti@lastminute.com>
2024-08-30 09:52:37 +0200

    Adds GSI to dynamoDB table

--- db.tf
+++ db.tf
@@ -2,27 +2,26 @@ resource "aws_dynamodb_table" "customer_table" {
   name         = "Customer"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ID"
-#   range_key = "username"
+  #   range_key = "username"
 
   attribute {
     name = "ID"
     type = "S"
   }
 
-#   attribute {
-#     name = "username"
-#     type = "S"
-#   }
+  attribute {
+    name = "username"
+    type = "S"
+  }
 
-  #   global_secondary_index {
-  #     name            = "UsernameIndex"
-  #     hash_key        = "name"      # GSI Partition key
-  #     projection_type = "ALL"
-  #
-  #     # Provisioned throughput for GSI (not needed for on-demand mode)
-  #     read_capacity  = 5
-  #     write_capacity = 5
-  #   }
+  global_secondary_index {
+    name            = "UsernameIndex"
+    hash_key        = "username"
+    projection_type = "ALL"
+
+    read_capacity  = 5
+    write_capacity = 5
+  }
 
   tags = {
     Name        = "Customer"
--- route.tf
+++ route.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "images-record" {
   name    = aws_s3_bucket.s3-bucket.bucket
   type    = "CNAME"
   records = [aws_s3_bucket.s3-bucket.bucket_regional_domain_name]
-  ttl = 600
+  ttl     = 600
 }
 
 # TODO use this if you want to use cloudfront

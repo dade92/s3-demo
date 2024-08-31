cf23d71d2ce4b5db2c9e6d5acdcb3994ff73d9ee
Davide Botti <davidebotti92@gmail.com>
2024-08-31 15:44:21 +0200

    Start to set up a cloudfront distribution

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,29 +1,38 @@
-# resource "aws_cloudfront_distribution" "my-distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.s3-bucket.bucket_regional_domain_name
-#     origin_id   = "S3-Origin"
-#   }
+# resource "aws_cloudfront_origin_access_control" "s3_oac" {
+#   name                              = "images-davidebotti-oac"
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
+# }
 #
+# resource "aws_cloudfront_distribution" "cdn" {
 #   enabled = true
 #
+#   origin {
+#     domain_name = aws_s3_bucket.s3-bucket.bucket_regional_domain_name
+#     origin_id   = "s3-${aws_s3_bucket.s3-bucket.bucket_domain_name}"
+#   }
+#
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+#
 #   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "S3-Origin"
-#
-#     viewer_protocol_policy = "allow-all"
-#
-#     min_ttl     = 0
-#     default_ttl = 3600
-#     max_ttl     = 86400
+#     allowed_methods        = ["GET", "HEAD"]
+#     cached_methods         = ["GET", "HEAD"]
+#     target_origin_id       = "s3-${aws_s3_bucket.s3-bucket.bucket_domain_name}"
+#     viewer_protocol_policy = "allow-all" # Allow both HTTP and HTTPS (HTTP only in practice since no cert)
 #
 #     forwarded_values {
 #       query_string = false
-#
 #       cookies {
 #         forward = "none"
 #       }
 #     }
+#
+#     min_ttl     = 0
+#     default_ttl = 3600
+#     max_ttl     = 86400
 #   }
 #
 #   restrictions {
@@ -32,11 +41,10 @@
 #     }
 #   }
 #
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
+#   aliases = ["images.davidebotti.com"]
 # }
 #
-# output "cloudfront_url" {
-#   value = aws_cloudfront_distribution.my-distribution.domain_name
-# }
+# #
+# # output "cloudfront_url" {
+# #   value = aws_cloudfront_distribution.my-distribution.domain_name
+# # }
--- route.tf
+++ route.tf
@@ -7,10 +7,14 @@ resource "aws_route53_record" "images-record" {
 }
 
 # TODO use this if you want to use cloudfront
-# resource "aws_route53_record" "images-record" {
+# resource "aws_route53_record" "cdn_record" {
 #   zone_id = "Z01401972E5NVI2LD2MZT"
 #   name    = "images.davidebotti.com"
-#   type    = "CNAME"
-#   records = [aws_cloudfront_distribution.my-distribution.domain_name]
-#   ttl = 600
+#   type    = "A"
+#
+#   alias {
+#     name                   = aws_cloudfront_distribution.cdn.domain_name
+#     zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+#     evaluate_target_health = false
+#   }
 # }
